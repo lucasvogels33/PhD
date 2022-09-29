6b623c156a7ae53c619ed4ef023e46ef2fe43509
Lucas Vogels <l.f.o.vogels@uva.nl>
2022-09-29 13:30:00 +0200

    test

---
+++ run_and_save.R
@@ -0,0 +1,34 @@
+#--read the arguments --- #
+args = commandArgs(trailingOnly = TRUE)
+n = as.numeric(args[1])
+p = as.numeric(args[2])
+graph = args[3]
+iter_rep = as.numeric(args[4])
+
+#--print the size of the problem in the command prompt 
+cat("the size of the problems is: ",n,"\n")
+
+#--download the necessary libraries and load the run_experiments file --
+library( BDgraph )
+source("run_experiments.R")
+
+#--set parameters to simulate data --#
+prob = 0.2
+type = "Gaussian"
+size = NULL
+vis = FALSE
+
+#--set parameters to solve data --#
+iter = 500
+burnin= iter/2
+jump = 1
+save = FALSE
+verbose = TRUE
+
+#-- run experiments
+result = sim_compute( p = p, n = n, graph = graph, type = type, vis = vis, 
+                      jump = jump, iter = iter, burnin = burnin, save = save, 
+                      verbose = verbose )
+
+#--print data to a Rdata file
+save( result, file = paste0( "testdata.Rdata" ) )
---
+++ run_experiments.R
@@ -0,0 +1,30 @@
+run_experiments = function( p = 10, n = 100, graph = "random", type = "Gaussian", 
+                        vis = FALSE, jump = 1, iter = 100000, burnin = 70000, 
+                        save = FALSE, verbose = TRUE )
+{
+set.seed(1)
+
+##-----simulate data-------------##
+
+data.sim = bdgraph.sim( p = p, n = n, graph = graph, prob = prob, size = size, type = type, vis = vis )
+
+#----solve data---------#
+t1_mpl_bd      = proc.time()	
+sample_mpl_bd  = bdgraph.mpl( data = data.sim, algorithm = "bdmcmc", iter = iter, burnin = burnin, jump = jump, save = save)
+time_mpl_bd    = as.numeric( ( proc.time() - t1_mpl_bd )[ 3 ] )
+
+compare_mpl_bd = compare( data.sim, sample_mpl_bd )[,2]
+plinks_mpl_bd  = as.matrix( sample_mpl_bd $ p_links, round = 10 )
+roc_mpl_bd     = BDgraph::roc( pred = sample_mpl_bd, actual = data.sim )
+auc_mpl_bd     = auc( roc_mpl_bd )[ 1 ]
+
+# save solutions results in a list
+return(list( time_mpl_bd = time_mpl_bd, compare_mpl_bd = compare_mpl_bd, plinks_mpl_bd = plinks_mpl_bd, roc_mpl_bd = roc_mpl_bd, auc_mpl_bd = auc_mpl_bd,true_g = as.matrix( data.sim $ G ) ) )
+
+
+
+
+
+
+
+}
