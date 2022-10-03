f27d519925fa319e3e5f1d1dd9e327a74c91fc70
Lucas Vogels <l.f.o.vogels@uva.nl>
2022-10-03 14:12:21 +0200

    test

--- results_sub.R
+++ results_sub.R
@@ -4,24 +4,53 @@ load_results = function(p=5,graph="random",n=10,round=2,iter_rep=10)
 {
     auc_mpl_bd = c(rep(0,iter_rep))
     time_mpl_bd = c(rep(0,iter_rep))
+    auc_bd_app= c(rep(0,iter_rep))
+    time_bd_app = c(rep(0,iter_rep))
+    auc_bd_ex = c(rep(0,iter_rep))
+    time_bd_ex = c(rep(0,iter_rep))
+    auc_rj_app = c(rep(0,iter_rep))
+    time_rj_app = c(rep(0,iter_rep))
+    auc_rj_ex = c(rep(0,iter_rep))
+    time_rj_ex = c(rep(0,iter_rep))
 
     
-    filename = paste0(graph,"_output_all")
+    filename_paste = paste0(graph,"_output_all")
     
     for (i in 1:iter_rep){
-        filename = paste0("result_p",p,"_n",n,"_",graph,"_rep",i,".Rdata")
-        load(file = filename)
+        filename_copy = paste0("result_p",p,"_n",n,"_",graph,"_rep",i,".Rdata")
+        load(file = filename_copy)
 
         auc_mpl_bd[i] = result $ auc_mpl_bd
         time_mpl_bd[i] = result $ time_mpl_bd
+        auc_bd_app[i]= result $ auc_bd_app
+        time_bd_app[i] = result $ time_bd_app
+        auc_bd_ex[i] = result $ auc_bd_ex
+        time_bd_ex[i] = result $ time_bd_ex
+        auc_rj_app[i] = result $ auc_rj_app
+        time_rj_app[i] = result $ time_rj_app
+        auc_rj_ex[i] = result $ auc_rj_ex
+        time_rj_ex[i] = result $ time_rj_ex
         
-        cat("auc of rep ",i," of the mpl_bd method is ",auc_mpl_bd[i]," ", file = filename,"\n", append = TRUE)
-        cat("time of rep ",i," of the mpl_bd method is ",time_mpl_bd[i]," ", file = filename,"\n", append = TRUE)
+        cat("auc of rep ",i," of the mpl_bd method is ",auc_mpl_bd[i]," ", file = filename_paste,"\n", append = TRUE)
+        cat("time of rep ",i," of the mpl_bd method is ",time_mpl_bd[i]," ", file = filename_paste,"\n", append = TRUE)
+        cat("auc of rep ",i," of the bd_app method is ",auc_bd_app[i]," ", file = filename_paste,"\n", append = TRUE)
+        cat("time of rep ",i," of the bd_app method is ",time_bd_app[i]," ", file = filename_paste,"\n", append = TRUE)
+        cat("auc of rep ",i," of the bd_ex method is ",auc_bd_ex[i]," ", file = filename_paste,"\n", append = TRUE)
+        cat("time of rep ",i," of the bd_ex method is ",time_bd_ex[i]," ", file = filename_paste,"\n", append = TRUE)
+        cat("auc of rep ",i," of the rj_app method is ",auc_rj_app[i]," ", file = filename_paste,"\n", append = TRUE)
+        cat("time of rep ",i," of the rj_app method is ",time_rj_app[i]," ", file = filename_paste,"\n", append = TRUE)
+        cat("auc of rep ",i," of the rj_ex method is ",auc_rj_ex[i]," ", file = filename_paste,"\n", append = TRUE)
+        cat("time of rep ",i," of the rj_ex method is ",time_rj_ex[i]," ", file = filename_paste,"\n", append = TRUE)
+        cat("\n",append=TRUE,file=filename_paste)
 
         
     }
     
-    return(list(auc_mpl_bd=auc_mpl_bd, time_mpl_bd = time_mpl_bd))
+    return(list(auc_mpl_bd=auc_mpl_bd, time_mpl_bd = time_mpl_bd,
+                auc_bd_app=auc_bd_app, time_bd_app = time_bd_app,
+                auc_bd_ex=auc_bd_ex, time_bd_ex = time_bd_ex,
+                auc_rj_app=auc_rj_app, time_rj_app = time_rj_app,
+                auc_rj_ex=auc_rj_ex, time_rj_ex = time_rj_ex))
     
 
 }
