4945d630bb6a4a768dbd30bc35a716b25f066c45
Lucas Vogels <l.f.o.vogels@uva.nl>
2022-11-23 15:45:11 +0100

    ddd

--- send_all_jobs.R
+++ send_all_jobs.R
@@ -25,10 +25,10 @@ create_script = function( n = 50, graph = "random", iter = 1, p = 10, constraint
 
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
 graph_list = c("random","cluster","scale-free") 
-n_list = c(50,100,500)
-p_list = c(50)
+n_list = c(100,200,1000)
+p_list = c(100)
 iter = 50
-walltime = "3-00:00:00"
+walltime = "4-23:59:00"
 partition = "normal" 
 nodes = 1
 ntasks = 1
